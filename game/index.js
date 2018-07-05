92c2c818893485f1df511ef926a345d97813b30a
Mallory Frantz <m.renae.frantz@gmail.com>
2018-07-05 13:04:43 -0400

    mals commit

--- game/index.js
+++ game/index.js
@@ -2,27 +2,27 @@ import { Map } from 'immutable'
 import redux from 'redux'
 
 const MOVE = 'move';
+let board = Map();
+const initialState = {board, turn: 'X'}
 
+export const move = (player, position)  => {
 
-// board = board.setIn([1, 1], 'X');
-
-let move = ((position, player) => {
-  let board = Map();
-  board.setIn(position, player)
+  // board = board.setIn([1, 1], 'X');
   return {
     type: MOVE,
     position: position,
     player: player
   }
-});
+};
 
-export default function reducer(state, action) {
+export default function reducer(state = initialState, action) {
+  const newState = Object.assign({}, state)
   switch (action.type) {
     case MOVE:
-      return state + action.position + action.player
+    newState.board = newState.board.setIn(action.position, action.player)
+    newState.turn = newState.turn === 'X' ? 'O' : 'X'
+      return  newState
     default:
       return state
   }
-};
-
-
+}
