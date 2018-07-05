94162027cb92631d0166acb795ef70fe10171aab
DC-1680 <dbconcha@gmail.com>
2018-07-05 16:42:08 -0400

    winner and streak looping

--- game/index.js
+++ game/index.js
@@ -1,5 +1,6 @@
 import { Map } from 'immutable'
 import redux from 'redux'
+// import { constants } from 'http2';
 
 const MOVE = 'move';
 let board = Map();
@@ -27,39 +28,49 @@ export default function reducer(state = initialState, action) {
   }
 }
 
-const winner = (board) => {
-//board.forEach()
-const coords = [
-  [0, 0], [0, 1], [0, 2],
-  [1, 0], [1, 1], [1, 2],
-  [2, 0], [2, 1], [2, 2]
-]
-for(let i = 0; i < 3; i++){
-  for(let j = 0; j < 3; j++){
-    if(i[j]  === 'X'){
-      
+export const winner = (board) => {
+  //board.forEach()
+  let row = (a, b) => ([[a, b], [a, b + 1], [a, b + 2]]);
+  let col = (a, b) => ([[a, b], [a + 1, b], [a + 2, b]]);
+  let diag1 = (a, b) => ([[a, b], [a + 1, b + 1], [a + 2, b + 2]]);
+  let diag2 = (a, b) => ([[a, b], [a + 1, b - 1], [a + 2, b - 2]]);
+
+  // const coords = [
+  //   [0, 0], [0, 1], [0, 2],
+  //   [1, 0], [1, 1], [1, 2],
+  //   [2, 0], [2, 1], [2, 2]
+  // ]
+
+  for (let i = 0; i < 3; i++) {
+    if (streak(board, row(i, i)) === "X" || streak(board, col(i, i)) === "X") {
+      return "X wins"
+    } else if (streak(board, row(i, i) === "O" || streak(board, col(i, i)) === "O")) {
+      return "O wins"
     }
   }
-}
-  if(){
-  //if streak winner returned
-  return 
-  } else if(board.includes(null)){
-  //no streak check if any null
-  //.includes(will check for null)
-    return null;
-  } else {
-    return 'draw';
+
+  if (streak(board, diag1(0, 0) === "X" || streak(board, diag2(0, 2) === "X"))) {
+    return "X wins";
+  } else if (streak(board, diag1(0, 0) === "O" || streak(board, diag2(0, 2) === "O"))) {
+    return "0 wins"
   }
 
+
+  // if (board.includes(null)) {
+  //   //no streak check if any null
+  //   //.includes(will check for null)
+  //   return null;
+  // }
+
+  return 'draw';
 }
 
 const streak = (board, firstCoord, ...remainingCoords) => {
   //helper func for checking the coords for either player
   let player = board.getIn(firstCoord);
   if (!player) return null;
-  for (let coord of remainingCoords){
-    if(board.getIn(coord) !== player) return null;
- }
- return player;
+  for (let coord of remainingCoords) {
+    if (board.getIn(coord) !== player) return null;
+  }
+  return player;
 }
